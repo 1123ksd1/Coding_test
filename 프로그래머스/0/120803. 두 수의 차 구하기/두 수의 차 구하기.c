a2dc10002332ad825758dd1606781bdd5ba01d23
Sodam Kim <129500149+1123ksd1@users.noreply.github.com>
2025-06-20 09:32:10 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    if(-5000<=num1<=5000 && -5000<=num2<=5000)
+    {
+        answer = num1-num2;
+    }
+    return answer;
+}
