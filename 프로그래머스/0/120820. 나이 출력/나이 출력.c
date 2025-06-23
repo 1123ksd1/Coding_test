243492f9f3185c69c18e84cb411b5bf7ff2786ec
Sodam Kim <129500149+1123ksd1@users.noreply.github.com>
2025-06-23 10:10:54 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.47 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+//2025 - 나이 
+int solution (int age)
+{
+    int answer= 0;
+    int year = 2022;
+    int birth = year - age + 1;
+    answer = birth ;
+    return answer;
+}
+
+    
